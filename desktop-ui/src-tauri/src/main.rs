3e8a36ac48746568724976f46aa88744759fb2ff
Fabian Althaus <fabian.althaus@rex-at.de>
2025-11-11 00:03:52 +0100

    add dialog plugin and permission

--- desktop-ui/src-tauri/src/main.rs
+++ desktop-ui/src-tauri/src/main.rs
@@ -284,6 +284,7 @@ fn find_typescript_files(dir: &Path, files: &mut Vec<String>) -> std::io::Result
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
             analyze_project,
             transpile_project,
